7215eff9ccc7faa1d288eab6d168650ffd2d2fdb
Mr Mukesh <88231908+ofmukesh@users.noreply.github.com>
2022-09-24 12:54:44 +0530

    Insertion Sort

---
+++ insertion.py
@@ -0,0 +1,12 @@
+def insertionSort(arr):
+  for i in range(len(arr)):
+    temp=arr[i]
+    j=i-1
+    while(j>=0 and temp < arr[j]):
+      arr[j+1]=arr[j]
+      arr[j]=temp
+      j-=1
+  print(arr)
+  
+  
+insertionSort([6,5,1,3])
