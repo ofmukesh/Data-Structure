040b68bf67ae02264f4a1090045fc431ff5e015a
Mr Mukesh <88231908+ofmukesh@users.noreply.github.com>
2022-09-25 09:07:46 +0530

    bubble Sort

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,8 +1,20 @@
 arr=[4,5,34,58,1,5]
 
+# bubbling smallest no. to the top
+
 for i in range(len(arr)-1,0,-1):
     temp=arr[i]
     if temp<arr[i-1]:
         arr[i]=arr[i-1]
         arr[i-1]=temp
+print(arr)
+
+# sorting array
+
+for i in range(len(arr)):
+    for j in range(len(arr)-1,i,-1):
+        temp=arr[j]
+        if temp<arr[j-1]:
+            arr[j]=arr[j-1]
+            arr[j-1]=temp
 print(arr)
